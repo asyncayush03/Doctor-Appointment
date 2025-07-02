c91eaadabe9dd28e57ccc45f8b22bd48125dbb35
ayushsharma031009 <ayushsomi2003@gmail.com>
2025-07-02 23:43:34 +0530

    used concurrently

---
+++ server.js
@@ -0,0 +1,28 @@
+const express = require('express');
+const colors = require('colors')
+const morgan = require('morgan')
+const dotenv = require('dotenv')
+
+
+dotenv.config()
+
+
+const app = express()
+
+
+app.use(express.json());
+app.use(morgan('dev'))
+
+
+app.get("/", (req, res)=>{
+    res.status(200).send({
+        message: "Server Running"
+    })
+});
+
+
+const port = process.env.PORT || 8080
+
+app.listen(port,()=>{
+    console.log(`Server Running in ${process.env.DEV_MODE} Mode on port : ${process.env.PORT}`.bgMagenta.white)
+})
