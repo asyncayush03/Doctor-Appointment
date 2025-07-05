ad75d3a7b5c83150b04e65a7b00dade3cd20f836
asyncayush03 <ayushsomi2003@gmail.com>
2025-07-06 02:43:14 +0530

    login done

--- server.js
+++ server.js
@@ -1,10 +1,14 @@
 const express = require('express');
 const colors = require('colors')
 const morgan = require('morgan')
-const dotenv = require('dotenv')
+const dotenv = require('dotenv');
+const connectDB = require('./config/db');
 
 
 dotenv.config()
+
+
+connectDB();
 
 
 const app = express()
@@ -14,15 +18,11 @@ app.use(express.json());
 app.use(morgan('dev'))
 
 
-app.get("/", (req, res)=>{
-    res.status(200).send({
-        message: "Server Running"
-    })
-});
-
+app.use('/api/v1/user',require("./routes/userRoutes"))
 
 const port = process.env.PORT || 8080
 
 app.listen(port,()=>{
     console.log(`Server Running in ${process.env.DEV_MODE} Mode on port : ${process.env.PORT}`.bgMagenta.white)
 })
+
